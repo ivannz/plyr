c9555bf3b3f9b29840a993e42c6124725cfe6b08
Ivan Nazarov <ivannz@yandex.ru>
2022-06-14 00:26:04 +0300

    decrease the minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 
 setup(
     name="python-plyr",
-    version="0.8.1",
+    version="0.8",
     description="""Mapping tools for nested containers.""",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
